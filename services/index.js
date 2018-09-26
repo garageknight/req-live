45ff4174b38b2725f4dc5d9270a66a92ea0fc9cd
garageknight <9707773+garageknight@users.noreply.github.com>
2018-09-26 01:35:39 +0100

    added jest

--- services/index.js
+++ services/index.js
@@ -1,5 +1,5 @@
 const config = require('./config');
 const logger = require('./logger');
 
-module.exports.logger = logger(config.has('Logger') ? config.get('Logger') : {});
+module.exports.logger = logger(config.get('Logger'));
 module.exports.config = config;
